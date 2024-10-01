095aa69bc9b271c37faad3fb6f8ede577e44eaf3
Muhammad Salihu <muhammad1.salihu@gmail.com>
2024-10-01 15:02:39 +0100

    fix: deployment fix

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router"
+
 
 const About = () => { 
     return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 const Home = () => { 
     return (
-    <div className="bg-green-100  flex flex-col justify-center items-center h-screen">
-      <h1 className="mb-4 text-8xl font-semibold font-sora text-green-600">Hello Umnafass!</h1>
-      <div className="flex gap-3">
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-        <a href="/store">Store</a>
-      </div>
-    </div>
+        <div className="w-full h-screen">
+            {/* Header */}
+            <div className="bg-green-800 h-[10%]">Header</div>
+
+            {/* Main Layout */}
+            <div className="bg-yellow-400 h-[90%]">
+                <div className="flex flex-col justify-center items-center h-screen">
+                    <h1 className="mb-4 text-8xl font-semibold font-sora text-green-600">Hello Umnafass!</h1>
+                        <div className="flex gap-3">
+                            <a href="/about">About</a>
+                            <a href="/contact">Contact</a>
+                            <a href="/store">Store</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
     )
  }
 
