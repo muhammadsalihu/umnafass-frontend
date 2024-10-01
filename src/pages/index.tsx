3837ac8430bed44a193710a554550f47459ba3c5
Muhammad Salihu <muhammad1.salihu@gmail.com>
2024-10-01 15:11:50 +0100

    fix: fixed routing issue in production

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
-const Home = () => { 
+import { useNavigate } from "react-router-dom"
+import logo from "../assets/logo.png"
+
+const Home = () => {
+    const navigate = useNavigate(); 
+
+    const goToAbout = () => { 
+        navigate('/about')
+     }
+
+    const goToContact = () => { 
+        navigate('/contact')
+     }
+
+    const goToStore = () => { 
+        navigate('/store')
+     }
     return (
         <div className="w-full h-screen">
             {/* Header */}
-            <div className="bg-green-800 h-[10%]">Header</div>
+            <div className="bg-green-800 h-[10%]">
+                <img src={logo} alt="umnafass-gifts-logo" />
+            </div>
 
             {/* Main Layout */}
             <div className="bg-yellow-400 h-[90%]">
                 <div className="flex flex-col justify-center items-center h-screen">
                     <h1 className="mb-4 text-8xl font-semibold font-sora text-green-600">Hello Umnafass!</h1>
                         <div className="flex gap-3">
-                            <a href="/about">About</a>
-                            <a href="/contact">Contact</a>
-                            <a href="/store">Store</a>
+                            <a href="#" onClick={goToAbout}>About</a>
+                            <a href="/contact" onClick={goToContact}>Contact</a>
+                            <a href="/store" onClick={goToStore}>Store</a>
                         </div>
                     </div>
                 </div>
