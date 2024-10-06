1a9800b9ff1111d624b67c80c6e116a3ebaa17d7
Muhammad Salihu <muhammad1.salihu@gmail.com>
2024-10-06 09:55:01 +0100

    feat: added the footer section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,23 @@ import helloIcon from "../assets/icons/hello container.png"
 import giftsPack from "../assets/images/giftsPack.png"
 import birthdayGiftPack from "../assets/images/birthdayGiftPack.png"
 import giftRow1  from "../assets/images/giftRow1.png"
+import fastShipping  from "../assets/icons/fastShipping.png"
+import secureCheckout  from "../assets/icons/secureCheckout.png"
+import helpCenter  from "../assets/icons/helpCenter.png"
+import instaSocial1  from "../assets/images/socialInsta1.png"
+import instaSocial2  from "../assets/images/socialInsta2.png"
+import instaSocial3  from "../assets/images/socialInsta3.png"
+import instaSocial4  from "../assets/images/socialInsta4.png"
+import instaSocial5  from "../assets/images/socialInsta5.png"
+import paypal  from "../assets/images/paypal.png"
+import mastercard  from "../assets/images/mastercard.png"
+import phone  from "../assets/icons/phone.png"
+import email  from "../assets/icons/email.png"
+import address  from "../assets/icons/address.png"
+import twitter  from "../assets/icons/twitterIcon.png"
+import tiktok  from "../assets/icons/tiktokIcon.png"
+import facebook  from "../assets/icons/facebookIcon.png"
+import instagram  from "../assets/icons/instagramIcon.png"
 
 
 const Hero = () => { 
@@ -79,8 +96,8 @@ const Home = () => {
     return (
         <div className="w-full h-screen">
             {/* Header */}
-            <div className="bg-[#0F220C] h-[10%] flex justify-between items-center px-24 text-white">
-                <div className="cursor-pointer" onClick={goToHome}>
+            <div className="flex-col md:flex-row   bg-[#0F220C] h-[15%] md:h-[10%] flex justify-between items-center py-2 md:py-0 px-24 text-white w-full">
+                <div className="cursor-pointer mb-1" onClick={goToHome}>
                     <img src={logo} alt="umnafass-gifts-logo" />
                 </div>
 
@@ -99,7 +116,6 @@ const Home = () => {
 
             {/* Main Layout */}
             <div className="h-[90%]">
-                    <div className="text-center py-2 bg-green-100 w-full">Sales! Sales! Sales</div>
                     <Hero />
                     <Cards />
 
@@ -123,11 +139,11 @@ const Home = () => {
                         </div>
 
                         {/* 2nd Row */}
-                        <div className="flex gap-4">
-                            <img src={giftRow1} alt="" />
-                            <img src={giftRow1} alt="" />
-                            <img src={giftRow1} alt="" />
-                            <img src={giftRow1} alt="" />
+                        <div className="flex-col md:flex-row flex gap-4">
+                        <ProductPreview />
+                        <ProductPreview />
+                        <ProductPreview />
+                        <ProductPreview />
                         </div>
 
                         {/* 3rd Row */}
@@ -144,10 +160,145 @@ const Home = () => {
                     <p className="text-center mb-4 text-4xl font-semibold">Over 1000 custom gifts created!</p>
                     <button className="text-center bg-black text-white text-xs px-4 py-2 rounded">Get your custom gift now</button>
                 </div> 
+
+                {/* Fast Shipping, Secured Checkout, Help Center */}
+                <div className="flex-col items-center gap-4 flex py-8 px-4">
+                    {/* Fast Shipping */}
+                    <div className="gap-4 p-2 flex ">
+                        <img src={fastShipping} alt="fast-shipping" />
+                        <div className="flex flex-col">
+                            <p className="text-sm font-bold">Fast Shipping</p>
+                            <p className="text-gray text-xs">Get your gift delivered in 24h</p>
+                        </div>
+                    </div>
+
+                    {/* Secured Checkout */}
+                    <div className="gap-4  p-2 flex ">
+                        <img src={secureCheckout} alt="fast-shipping" />
+                        <div className="flex flex-col">
+                            <p className="text-sm font-bold">Secured Checkout</p>
+                            <p className="text-gray text-xs">Get your gift delivered in 24h</p>
+                        </div>
+                    </div>
+
+                    {/* Help Center */}
+                    <div className="gap-4  p-2 flex ">
+                        <img src={helpCenter} alt="fast-shipping" />
+                        <div className="flex flex-col">
+                            <p className="text-sm font-bold">Fast Shipping</p>
+                            <p className="text-gray text-xs">Get your gift delivered in 24h</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Buttons Section */}
+                <div className="text-center  mb-14">
+                    <button className="border border-gray-400 text-sm rounded-full px-10 py-2 shadow">Customize a Gift Now</button>
+                    <button className="border border-gray-400 text-sm rounded-full px-10 py-2 shadow">Follow Us</button>
+                    <button className="bg-green-900 text-sm text-white rounded-full px-10 py-2 shadow">Get your custom gift now</button>
+                    <button className="bg-green-500 text-sm text-white rounded-full px-10 py-2 shadow">Buy Now</button>
+                </div>
+
+
+                {/* Social Media Section */}
+                <div className="mb-20 w-full">
+                    {/* Follow Us */}
+                    <div className="mb-10 flex justify-between w-full items-center px-8 md:px-14 ">
+                        <div className="">
+                            <p className="text-2xl font-bold mb-2">Follow Us on Instagram</p>
+                            <p className="text-xs">Lorem ipsumm, Lorem dis tie lorem ispaohdg</p>
+                        </div>
+                        
+                        <button className="border border-gray-400 text-sm rounded-full px-10 py-2 shadow">Follow Us</button>
+                    </div>
+
+                    {/* Instagram Picture */}
+                    <div className="flex flex-col lg:flex-row px-14 ">
+                        {/* left */}
+                        <div className="flex flex-col w-[50%]">
+                            <div className="flex">
+                                <img src={instaSocial1} alt="insta" />
+                                <img src={instaSocial2} alt="insta" />
+                            </div>
+                            <div className="flex">
+                                <img src={instaSocial3} alt="insta" />
+                                <img src={instaSocial4} alt="insta" />
+                            </div>
+                            
+                        </div>
+
+
+                        {/* Right */}
+                        <div className="">
+                            <img src={instaSocial5} alt="insta" />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Footer Section */}
+                <div className="bg-[#10290A] flex flex-col md:flex-row md:justify-between p-10">
+                    {/* Column One */}
+                    <div className="mb-6">
+                        <img className="mb-4" src={logo} alt="logo" />
+                        <div className="flex gap-2">
+                            <img src={paypal} alt="paypal" />
+                            <img src={mastercard} alt="mastercard" />
+                        </div>
+                    </div>
+
+                    {/* Middle Section  */}
+                    <div className="flex flex-col md:gap-8 md:flex-row">
+                        {/* Column Two */}        
+                        <div className="flex flex-col mb-4">
+                            <p className="text-white font-semibold text-lg mb-4">Menu</p>
+                            <p className="text-white  text-sm mb-1">Home</p>
+                            <p className="text-white  text-sm mb-1">Products</p>
+                            <p className="text-white  text-sm mb-1">Gallery</p>
+                        </div>
+
+                        {/* Column Three */}
+                        <div className="flex flex-col mb-4">
+                            <p className="text-white font-semibold text-lg mb-4">Resources</p>
+                            <p className="text-white  text-sm mb-1">Store Policies</p>
+                            <p className="text-white  text-sm mb-1">Find my order</p>   
+                        </div>
+
+                        {/* Column Four */}
+                        <div className="flex flex-col mb-4">
+                            <p className="text-white font-semibold text-lg mb-4">Contact</p>
+                            <div className="flex">
+                                <img className="mr-2" src={phone} alt="phone" />    
+                                <p className="text-white  text-sm mb-1">08079454558</p>
+                            </div>
+                            <div className="flex">
+                                <img className="mr-2" src={email} alt="email" />    
+                                <p className="text-white  text-sm mb-1">sales@umnafass.com</p>
+                            </div>
+                            <div className="flex">
+                                <img className="mr-2" src={address} alt="address" />    
+                                <p className="text-white  text-sm mb-1">Abuja, Nigeria</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+
+                    {/* Column Five */}
+                    <div className="flex flex-col">
+                        <p className="text-white font-semibold text-lg mb-4">Social Media</p>
+                        <div className="flex gap-4">
+                            <img className="mr-2" src={instagram} alt="instagram" />    
+                            <img className="mr-2" src={twitter} alt="twitter" />    
+                            <img className="mr-2" src={tiktok} alt="tiktok" />    
+                            <img className="mr-2" src={facebook} alt="facebook" />    
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+                
             </div>
-
-
-               
+      
         </div>
     )
  }
